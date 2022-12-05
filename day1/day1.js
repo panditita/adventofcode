fa5dded35c63a279dc8a284bfc2652f68b0b680c
Etzali <etzali@panditita.uk>
2022-12-05 20:54:26 +0000

    :art: changes parameter name

--- day1/day1.js
+++ day1/day1.js
@@ -6,11 +6,11 @@ let sortCalories = constants.INPUT.split("\n\n")
     return elf
       .split("\n") //create array for each elfs
       .map((cal) => Number(cal)) //convert calories into integers
-      .reduce((previous, current) => previous + current); //add calories to get total
+      .reduce((accumulator, current) => accumulator + current); //add calories to get total
   })
   .sort((calA, calB) => calB - calA) //order calories from highest to lowest
   // part 2
   .slice(0, 3) //slice the array by the first 3 values
-  .reduce((previous, current) => previous + current); //add the three highest calories to get total
+  .reduce((accumulator, current) => accumulator + current); //add the three highest calories to get total
 
 console.log(sortCalories);
